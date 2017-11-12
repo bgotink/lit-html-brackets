b56b765b45752b7a542c2f751e8e07ad1086a2e9
Bram Gotink <bram@gotink.me>
2017-11-12 20:41:44 +0100

    {chore} Rework build/publish scripts
    - build:dev will build all files using typescript
    - build:test will build the test files
    - build:lib will build the library and run rollup to produce a single file
    - prepare compiles the tests and the library and removes any dev build
      artefacts.

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import * as path from 'path';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+
+export default {
+ input: 'out-tsc/lit-html-brackets.js',
+
+ external: [
+   path.resolve(__dirname, 'lit-html', 'lit-html.js'),
+ ],
+
+ plugins: [
+   sourcemaps(),
+ ],
+
+ output: {
+   file: './lit-html-brackets.js',
+   format: 'es',
+   sourcemap: true,
+ }
+};
